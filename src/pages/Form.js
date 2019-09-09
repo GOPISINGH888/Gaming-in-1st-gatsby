7c2260362b51d91ec5f9219ff843504d018e1bc4
unknown <officialgopisingh@gmail.com>
2019-09-09 14:23:41 +0530

    contact form fixed

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,94 +1,116 @@
 import React from "react"
 import styled from "styled-components"
 import Banner from "../components/Banner"
-const Title = styled.p`
-  margin-top: 5px;
-  margin-bottom: 10px;
-  margin-left: 5px;
+const Title = styled.div`
+  margin-bottom: 30px;
+  color: #ffb320;
 `
-const Bgclr = styled.p`
-  background-color: lightcyan;
+const Box = styled.div`
+  margin-top: 40px;
+`
+const Bgimg = styled.div`
+  background: url(https://colorlib.com/preview/theme/gamewarrior/img/pattern.png);
+`
+const Clr = styled.p`
+  color: #ffb320;
+`
+const Button = styled.a`
+  background-color: #ffb320;
+  color: black;
 `
 export default () => (
-  <Bgclr>
+  <div>
     <Banner />
-    <div className="container">
-      <Title className=" has-text-weight-bold has-text-link">Sign in</Title>
-      <div className="columns">
-        <div className="column is-half">
-          <div className="field">
-            <label className="label  has-text-link">Username</label>
-            <div className="control">
-              <input
-                className=" input is-rounded"
-                type="text"
-                placeholder="e.g Username"
-              />
-            </div>
+    <Bgimg>
+      {" "}
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <Box>
+              <div className="box has-background-grey-dark">
+                <Title className=" has-text-weight-bold is-2">Log in</Title>
+                <div className="field">
+                  <label className="label has-text-weight-semibold">
+                    {" "}
+                    <Clr>Username </Clr>
+                  </label>
+
+                  <div className="control">
+                    <input
+                      className=" input is-rounded"
+                      type="text"
+                      placeholder="e.g Username"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label has-text-weight-semibold">
+                    <Clr>Password</Clr>
+                  </label>
+
+                  <div className="control">
+                    <input
+                      className="input is-rounded"
+                      type="password"
+                      placeholder="password"
+                    />
+                  </div>
+                </div>
+                <br />
+
+                <Button className="button  is-rounded ">Log in</Button>
+              </div>
+            </Box>
+            <Box>
+              {" "}
+              <div className="box has-background-grey-dark">
+                <Title className=" has-text-weight-bold  is-2">Sign up</Title>
+                <div className="field">
+                  <label className="label  has-text-weight-semibold">
+                    <Clr>Firstname</Clr>
+                  </label>
+
+                  <div className="control">
+                    <input
+                      className=" input is-rounded"
+                      type="text"
+                      placeholder="e.g Firstname"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label has-text-weight-semibold">
+                    <Clr>Lastname </Clr>
+                  </label>
+
+                  <div className="control">
+                    <input
+                      className=" input is-rounded"
+                      type="text"
+                      placeholder="e.g lastname"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label  has-text-weight-semibold">
+                    {" "}
+                    <Clr>Password </Clr>
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input is-rounded"
+                      type="password"
+                      placeholder="password"
+                    />
+                  </div>
+                </div>
+                <br />
+                <Button className="button is-rounded">Sign up</Button>
+              </div>
+            </Box>
           </div>
-          <div className="field">
-            <label className="label  has-text-link">Password</label>
-            <div className="control">
-              <input
-                className="input is-rounded"
-                type="password"
-                placeholder="password"
-              />
-            </div>
-          </div>
-          <label className="checkbox">
-            <input type="checkbox" />
-            Keep me signed in
-          </label>{" "}
-          <br />
-          <a className="button is-link is-rounded ">Sign in</a>
-          <hr />
-          <a>Forget password?</a>
-          <hr />
-          <Title className=" has-text-weight-bold  has-text-link ">
-            Sign up
-          </Title>
-          <div className="field">
-            <label className="label  has-text-link">Firstname</label>
-            <div className="control">
-              <input
-                className=" input is-rounded"
-                type="text"
-                placeholder="e.g Firstname"
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label  has-text-link">Lastname</label>
-            <div className="control">
-              <input
-                className=" input is-rounded"
-                type="text"
-                placeholder="e.g lastname"
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label  has-text-link">Password</label>
-            <div className="control">
-              <input
-                className="input is-rounded"
-                type="password"
-                placeholder="password"
-              />
-            </div>
-            <label className="checkbox">
-              <input className=" has-text-link" type="checkbox" />
-              show password
-            </label>
-          </div>
-          <label className="checkbox">
-            <input type="checkbox" />I accept all terms and conditions
-          </label>
-          <br />
-          <a className="button is-link is-rounded">Sign up</a>
         </div>
       </div>
-    </div>
-  </Bgclr>
+    </Bgimg>
+  </div>
 )
