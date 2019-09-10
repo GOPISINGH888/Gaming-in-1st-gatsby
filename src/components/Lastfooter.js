7b7f447c5dacc7f2a9bea6532afaabb0d477a02a
unknown <officialgopisingh@gmail.com>
2019-09-10 13:05:49 +0530

    lastfooter fixed

--- src/components/Lastfooter.js
+++ src/components/Lastfooter.js
@@ -7,7 +7,7 @@ const Containr = styled.div`
   background-color: black;
 `
 export default () => (
-  <div className="foter is-hidden-disktop">
+  <div className="foter is-hidden-desktop">
     <Containr>
       <div className="columns">
         <div className="column">
